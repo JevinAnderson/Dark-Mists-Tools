42e2b6410663b2efa41983ead0477ce5057b0e34
Jevin Anderson <jevinanderson@gmail.com>
2018-01-06 17:02:12 -0800

    Updated build configuration, added autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,13 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: 'index_bundle.js',
-    publicPath: ''
+    publicPath: '/'
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] }
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'] }
     ]
   },
   devServer: {
@@ -22,17 +22,17 @@ const config = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
     })
   ]
 };
 
 if (process.env.NODE_ENV === 'production') {
   config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
     new webpack.optimize.UglifyJsPlugin()
   );
 }
