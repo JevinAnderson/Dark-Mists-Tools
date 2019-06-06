c30be1be6cfb9d00be0ad7e0bffa7abb8c81014b
Jevin Anderson <jevanderson@paypal.com>
2019-06-05 18:12:40 -0700

    Added open logic

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const open = require('open');
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static('./docs'))
+app.use(express.static('./docs'));
 
+const port = 12345;
 app.listen(12345, error => {
   if (error) {
     console.log('Application startup error', error);
     process.exit(-1);
   }
 
-  console.log('Application started. Go to http://127.0.0.1:12345 in your browser.');
+  const url = `http://127.0.0.1:${port}`;
+
+  console.log(`Application started. Go to ${url} in your browser.`);
+  open(url);
 });
